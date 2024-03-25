7836ed8e3c182fa07437eb3d264a33558164ceb7
kishore2114 <kishor9102309@gmail.com>
2024-03-25 12:58:37 +0530

    d

---
+++ javascript.js
@@ -0,0 +1,56 @@
+console.log("Hello Javascript ")
+console.log(1+2)
+//const , var ,let
+const pi=3.1415;
+//pi=6.28
+var x=1;
+var y =1.4
+var z="hello";
+var a=[x,y,z];
+console.log(a)
+var lst = [1,"0",[1,2],4.8,true];
+console.log(lst)
+var obj={
+    name:"Mr x",
+    job :"Hacker",
+    age:23,
+    address:"bengaluru"
+}
+console.log(obj)
+var phone={
+    name:"xiaomi 11i",
+    color:["black", "purple","white"],
+    company:"xiaomi",
+    size:["128GB","256GB"]
+}
+console.log(phone)
+let str1='Hello';
+let str2="world";
+let str3=`hello 
+world`;
+console.log(str1)
+console.log(str2)
+console.log(str3)
+
+let outcome=[[1,2,3,4,5,6],
+             [2,3,4,5,6,7],
+             [3,4,5,6,7,8],
+             [4,5,6,7,8,9],
+             [5,6,7,8,9,0],
+             [6,7,8,9,0,1]]
+console.log(outcome[4][3])
+
+let D3=[
+        [
+            [1,2],
+            [1,2]
+        ],
+        [
+            [1,2],
+            [1,2]
+        ],
+        [
+            [1.2],
+            [1,2]
+        ]
+    ]//3*2
